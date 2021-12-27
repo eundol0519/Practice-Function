eb0467e0728465cacc653e529c25521a3810a20b
eundol0519 <72530436+eundol0519@users.noreply.github.com>
2021-12-27 19:02:14 +0900

    녹음 + 재생 기능 업그레이드

--- App.js
+++ App.js
@@ -1,46 +1,107 @@
 import "./App.css";
-import React from "react";
-import MapContainer from "./MapContainer";
-import MapContainer2 from "./MapContainer2";
-import MapContainer3 from "./MapContainer3";
-import Music from "./Music";
-import Music2 from "./Music2";
-import Timer from "./Timer";
-
-import { Route, Link } from "react-router-dom";
+import React, { useState, useCallback } from "react";
 
 function App() {
+  const [stream, setStream] = useState();
+  const [media, setMedia] = useState();
+  const [onRec, setOnRec] = useState(true);
+  const [source, setSource] = useState();
+  const [analyser, setAnalyser] = useState();
+  const [audioUrl, setAudioUrl] = useState();
+  const [disabled, setDisabled] = useState(true);
+
+  const onRecAudio = () => {
+
+    setDisabled(true)
+    // 음원정보를 담은 노드를 생성하거나 음원을 실행또는 디코딩 시키는 일을 한다
+    const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    // 자바스크립트를 통해 음원의 진행상태에 직접접근에 사용된다.
+    const analyser = audioCtx.createScriptProcessor(0, 1, 1);
+    setAnalyser(analyser);
+
+    function makeSound(stream) {
+      // 내 컴퓨터의 마이크나 다른 소스를 통해 발생한 오디오 스트림의 정보를 보여준다.
+      const source = audioCtx.createMediaStreamSource(stream);
+      setSource(source);
+      source.connect(analyser);
+      analyser.connect(audioCtx.destination);
+    }
+    // 마이크 사용 권한 획득
+    navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+      const mediaRecorder = new MediaRecorder(stream);
+      mediaRecorder.start();
+      setStream(stream);
+      setMedia(mediaRecorder);
+      makeSound(stream);
+
+      analyser.onaudioprocess = function (e) {
+        // 3분(180초) 지나면 자동으로 음성 저장 및 녹음 중지
+        if (e.playbackTime > 180) {
+          stream.getAudioTracks().forEach(function (track) {
+            track.stop();
+          });
+          mediaRecorder.stop();
+          // 메서드가 호출 된 노드 연결 해제
+          analyser.disconnect();
+          audioCtx.createMediaStreamSource(stream).disconnect();
+
+          mediaRecorder.ondataavailable = function (e) {
+            setAudioUrl(e.data);
+            setOnRec(true);
+          };
+        } else {
+          setOnRec(false);
+        }
+      };
+    });
+  };
+
+  // 사용자가 음성 녹음을 중지 했을 때
+  const offRecAudio = () => {
+    // dataavailable 이벤트로 Blob 데이터에 대한 응답을 받을 수 있음
+    media.ondataavailable = function (e) {
+      setAudioUrl(e.data);
+      setOnRec(true);
+    };
+
+    // 모든 트랙에서 stop()을 호출해 오디오 스트림을 정지
+    stream.getAudioTracks().forEach(function (track) {
+      track.stop();
+    });
+
+    // 미디어 캡처 중지
+    media.stop();
+
+    // 메서드가 호출 된 노드 연결 해제
+    analyser.disconnect();
+    source.disconnect();
+    
+    if (audioUrl) {
+      URL.createObjectURL(audioUrl); // 출력된 링크에서 녹음된 오디오 확인 가능
+    }
+    
+    // File 생성자를 사용해 파일로 변환
+    const sound = new File([audioUrl], "soundBlob", {
+      lastModified: new Date().getTime(),
+      type: "audio",
+    });
+  
+    setDisabled(false);
+    console.log(sound); // File 정보 출력
+  };
+
+  const play = () => {
+    const audio = new Audio(URL.createObjectURL(audioUrl));
+    audio.loop = false;
+    audio.volume = 1;
+    audio.play();
+  };
+
   return (
-    <div className="App">
-      <Link to={"/container"}>시도1</Link> &nbsp;
-      <Link to={"/container2"}>시도2</Link> &nbsp;
-      <Link to={"/container3"}>마커 이벤트 추가</Link> &nbsp;
-      <Link to={"/multiFile"}>파일 여러 개 올리기</Link>
-      <Link to={"/timer"}>타이머</Link>
-
-      <br></br>
-      <br></br>
-      <br></br>
-
-      <Music
-        urls={[
-          'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
-          'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3',
-          'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3',
-        ]}
-      />
-
-      <br/>
-      <br/>
-
-      <Music2></Music2>
-      
-      <Route path="/container" exact component={MapContainer}></Route>
-      <Route path="/container2" exact component={MapContainer2}></Route>
-      <Route path="/container3" exact component={MapContainer3}></Route>
-      <Route path="/music" exact component={Music}></Route>
-      <Route path="/timer" exact component={Timer}></Route>
-    </div>
+    <>
+      <button onClick={onRec ? onRecAudio : offRecAudio}>녹음</button>
+      <button onClick={play} disabled={disabled}>재생</button>
+    </>
   );
 }
 
